4a745f327b58e20d2ec12eeabf99ec288e74c208
Jürgen Tihanov <jyrgentihanov@gmail.com>
2023-09-17 15:36:17 +0300

    Added support for persistent undo and removed swap files

--- after/plugin/undotree.lua
+++ after/plugin/undotree.lua
@@ -1,3 +1,18 @@
 -- Undo tree config
 
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+
+
+-- Set persistent undo
+if vim.fn.has("persistent_undo") == 1 then
+    local target_path = vim.fn.expand("~/.undodir")
+
+    -- Create the directory and any parent directories if the location does not exist
+    if vim.fn.isdirectory(target_path) == 0 then
+        vim.fn.mkdir(target_path, "p", 0700)
+    end
+
+    vim.o.undodir = target_path
+    vim.o.undofile = true
+    vim.o.swapfile = false  -- Fuck swapfiles
+end
