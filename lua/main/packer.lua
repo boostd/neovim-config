240219095437c8cfd6971aeecc371a7d5944a8ab
Jürgen Tihanov <jyrgentihanov@gmail.com>
2023-10-10 16:25:10 +0300

    Removed redundant code

--- lua/main/packer.lua
+++ lua/main/packer.lua
@@ -17,10 +17,6 @@ local packer_bootstrap = ensure_packer()
 
 
 
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
-
-
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
