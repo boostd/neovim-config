f09a1bee68be394ff24bed046a6c863d9c32a612
Jürgen Tihanov <jyrgentihanov@gmail.com>
2023-11-17 09:50:06 +0200

    scroll 1 line remap

--- lua/boostd/remap.lua
+++ lua/boostd/remap.lua
@@ -39,6 +39,9 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 -- Make file executable (only works on UNIX like systems!)
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
+-- Add scroll 1 line up functionality to <C-g>
+vim.keymap.set("n", "<C-g>", "<C-e>")
+
 -- Quickly source the current file into config
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
