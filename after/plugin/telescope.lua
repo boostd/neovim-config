e8054f47c83ab8d87b2f0dbbebf40688137baee6
boostd <59803429+boostd@users.noreply.github.com>
2023-12-01 22:15:58 +0200

    typo fix

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -18,7 +18,7 @@ local builtin = require('telescope.builtin')
 -- Project search all files
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 
--- Fuzzy find just files in git
+-- Fuzzy find files only in git
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 
 -- Grep live
