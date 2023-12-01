b593964e88933ea076f68863639a803561dbc582
boostd <59803429+boostd@users.noreply.github.com>
2023-12-01 20:55:24 +0200

    Made grep better. Selection can be grepped and live grep is now used

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -8,9 +8,27 @@ vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 -- Fuzzy find just files in git
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 
--- Project search
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+-- Grep live
+vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
+
+-- Grep selection
+vim.keymap.set('x', '<leader>ps', function()
+  local text = vim.getVisualSelection()
+  builtin.grep_string({ default_text = text })
 end)
 
+
+-- Function to get the current selected text
+function vim.getVisualSelection()
+	vim.cmd('noau normal! "vy"')
+	local text = vim.fn.getreg('v')
+	vim.fn.setreg('v', {})
+
+	text = string.gsub(text, "\n", "")
+	if #text > 0 then
+		return text
+	else
+		return ''
+	end
+end
 
