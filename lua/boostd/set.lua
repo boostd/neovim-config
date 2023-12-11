78a9459051e434f90481ce73bdb7dfa1569f49ad
boostd <59803429+boostd@users.noreply.github.com>
2023-12-11 22:37:23 +0200

    Highlighting yanked text

--- lua/boostd/set.lua
+++ lua/boostd/set.lua
@@ -61,6 +61,16 @@ vim.opt.colorcolumn = "140"
 -- Map leader key to spacebar
 vim.g.mapleader = " "
 
+-- Highlight yanked text
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = vim.api.nvim_create_augroup('highlight_yank', {}),
+  desc = 'Hightlight selection on yank',
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank { higroup = 'IncSearch', timeout = 300 }
+  end,
+})
+
 
 
 
