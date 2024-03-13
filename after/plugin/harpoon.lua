6ee92426e716eb9909f9859d2e143ea5556ed715
Jürgen Tihanov <jyrgentihanov@gmail.com>
2024-03-13 23:13:05 +0200

    fixed harpoon list saving

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -2,7 +2,11 @@
 
 local harpoon = require("harpoon")
 -- REQUIRED
-harpoon:setup()
+harpoon:setup({
+  settings = {
+    save_on_toggle = true
+  }
+})
 -- REQUIRED
 
 vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
